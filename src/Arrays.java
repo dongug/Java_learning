18987eb5736f882ad09402baba714366dd44408b
Akmal Rizaev <testing300bucks@gmail.com>
2024-07-08 19:03:07 +0500

    arrays added part 1

---
+++ src/Arrays.java
@@ -0,0 +1,13 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[] intArray = new int[5];
+        //[0, 0, 0, 0, 0]
+
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = i;
+        }
+        String[] strArray = new String[5];
+        //[null, null, null, null, null]
+
+    }
+}
