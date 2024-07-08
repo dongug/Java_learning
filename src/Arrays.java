d48df77698f664b35a2d514b1e7c5f745d5daccb
dongug <rizakm1999@gmail.com>
2024-07-08 23:41:42 +0500

    arrays part 2

--- src/Arrays.java
+++ src/Arrays.java
@@ -5,9 +5,24 @@ public class Arrays {
 
         for (int i = 0; i < intArray.length; i++) {
             intArray[i] = i;
+            System.out.println(intArray[i]);
         }
-        String[] strArray = new String[5];
+        String[] strArray = {"One", "Two", "Three"};
+        for (int i = 0; i < strArray.length; i++) {
+            System.out.println(strArray[i]);
+        }
+        strArray[1] = "Five";
+        for (int i = 0; i < strArray.length; i++) {
+            System.out.println(strArray[i]);
+        }
         //[null, null, null, null, null]
+        int[][] newIntArray = new int[5][5];
 
+        for (int i = 0; i < newIntArray.length; i++) {
+            for (int j = 0; j < newIntArray[i].length; j++) {
+                System.out.print(newIntArray[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
